f244881824d9f75e651793629abb0cd0c9101abb
Nireeksha Huns <nireekshahuns@Nireekshas-Laptop.local>
2025-04-04 22:48:40 -0400

    Completed Postgresql integration and customer entity setup

--- src/main/java/com/riskshield/riskshield/CustomerController.java
+++ src/main/java/com/riskshield/riskshield/CustomerController.java
@@ -1,13 +1,18 @@
 package com.riskshield.riskshield;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @PostMapping
-    public String createCustomer(@RequestBody String customer) {
-        return "Customer has been received: " + customer;
+    public String createCustomer(@RequestBody Customer customer) {
+        customerRepository.save(customer);
+        return "Customer saved with name: " + customer.getName();
     }
 }
